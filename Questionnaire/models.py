a20e9ae29f72757495ab1c2298c80de4904178f5
AdityaKapoor74 <adityakapoor074@gmail.com>
2020-05-21 00:04:59 +0530

    adding Test tables

--- Questionnaire/models.py
+++ Questionnaire/models.py
@@ -105,3 +105,43 @@ class Classify_And_Learn_Samples_set5(models.Model):
 
     sample_img = models.ImageField(upload_to='images/')
     sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
+
+class Test_set1(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Classify and Learn Samples Set 1"
+
+    sample_img = models.ImageField(upload_to='images/')
+    sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
+
+class Test_set2(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Classify and Learn Samples Set 2"
+
+    sample_img = models.ImageField(upload_to='images/')
+    sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
+
+class Test_set3(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Classify and Learn Samples Set 3"
+
+    sample_img = models.ImageField(upload_to='images/')
+    sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
+
+class Test_set4(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Classify and Learn Samples Set 4"
+
+    sample_img = models.ImageField(upload_to='images/')
+    sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
+
+class Test_set5(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Classify and Learn Samples Set 5"
+
+    sample_img = models.ImageField(upload_to='images/')
+    sample_label = models.CharField(max_length=10,blank=True,null=True,default=None)
