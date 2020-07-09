3c8f33cedc738e707a6f7a50596fdf15972033ac
AdityaKapoor74 <adityakapoor074@gmail.com>
2020-07-09 23:26:29 +0530

    adding other questionnaire

--- QuestionnaireColorCue/apps.py
+++ QuestionnaireColorCue/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class QuestionnaireConfig(AppConfig):
-    name = 'Questionnaire'
+class QuestionnaireColorCueConfig(AppConfig):
+    name = 'QuestionnaireColorCue'
