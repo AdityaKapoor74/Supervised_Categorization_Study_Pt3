ad3fbc68b60abdc1524faa67f0c27ea796bb47cf
Aditya Kapoor <adityakapoor074@gmail.com>
2020-09-06 13:26:27 +0530

    removing images

--- Supervised_Categorization_Research/settings.py
+++ Supervised_Categorization_Research/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gz$2j*msxfl4eb2qm-z^_4%4z(up7_&97i1qjjig8=aw8pef+u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','128.199.21.87']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'Supervised_Categorization_Research.wsgi.application'
 
 DATABASES = {
     # SQL
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    # POSTGRES
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'userinfo',
-        'USER': 'aditya',
-        'PASSWORD': 'Iron42Patriot',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # POSTGRES
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'userinfo',
+    #     'USER': 'aditya',
+    #     'PASSWORD': 'Iron42Patriot',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
@@ -128,8 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
